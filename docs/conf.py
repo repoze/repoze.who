12dd4016d9c70d74db01b9f6ec2bb58d134b1289
Tres Seaver <tseaver@agendaless.com>
2010-03-26 20:21:39 +0000

    Automate API extraction.

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,11 @@ for item in os.listdir(parent):
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'repoze.sphinx.autointerface',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
